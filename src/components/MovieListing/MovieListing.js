44d6fd95e37ab28a8f2857246f74abd64a04a840
Abhishek suman <77213882+Abhishek-Sumn@users.noreply.github.com>
2023-01-02 10:31:02 +0530

    stage1completeoutof3

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { getAllMovies } from '../../features/movies/movieSlice'
+import { useSelector } from 'react-redux'
+import "../MovieListing/MovieListing.scss"
+import MovieCard from "../MovieCard/MovieCard"
 
 const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+
+  renderMovies = movies.Response === "True" ?
+    (movies.Search.map((movie, index) => {
+      return <MovieCard key={index} data={movie} />;
+    }))
+    : (<div className="movies-error"><h3>{movies.error}</h3></div >)
+
+
   return (
-    <div>MovieListing</div>
+    <div className='movie-wrapper'>
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
   )
 }
 
