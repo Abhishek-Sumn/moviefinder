3666054f8430ee24952fe10f14ad516dc133cdbe
Abhishek suman <77213882+Abhishek-Sumn@users.noreply.github.com>
2023-01-03 13:24:38 +0530

    shows added

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { getAllMovies } from '../../features/movies/movieSlice'
+import { getAllMovies, getAllShows } from '../../features/movies/movieSlice'
 import { useSelector } from 'react-redux'
 import "../MovieListing/MovieListing.scss"
 import MovieCard from "../MovieCard/MovieCard"
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
+
   let renderMovies = "";
+  let renderShows = "";
+
 
   renderMovies = movies.Response === "True" ?
     (movies.Search.map((movie, index) => {
@@ -14,12 +18,24 @@ const MovieListing = () => {
     }))
     : (<div className="movies-error"><h3>{movies.error}</h3></div >)
 
+  renderShows = shows.Response === "True" ?
+    (shows.Search.map((shows, index) => {
+      return <MovieCard key={index} data={shows} />;
+    }))
+    : (<div className="movies-error"><h3>{shows.error}</h3></div >)
+
+
 
   return (
     <div className='movie-wrapper'>
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   )
