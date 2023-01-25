0dcb49b9f49009c225698d09a1a315d246be4517
Abhishek suman <77213882+Abhishek-Sumn@users.noreply.github.com>
2023-01-25 22:00:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './features/store';
 import App from './App';
-//1
+//2
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
